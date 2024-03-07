5e6801d203857428a7c618bd364fb0e891fbef6b
Emily Raspe <emilyraspe@icloud.com>
2024-03-07 15:56:11 +0100

    add entries and maped them

--- src/components/Main/Entries.jsx
+++ src/components/Main/Entries.jsx
@@ -3,22 +3,50 @@ import "./Entries.css";
 import "../Button/Button.css";
 
 export default function Entries() {
+  const entries = [
+    {
+      id: 1000,
+      date: "Feb 5, 2025",
+      motto: "We are in a state of chaos",
+      notes:
+        "Today I learned about React State. It was fun! I can't wait to learn more.",
+    },
+    {
+      id: 999,
+      date: "Feb 4, 2025",
+      motto: "Props, Props, Props",
+      notes:
+        "Today I learned about React Props. Mad props to everyone who understands this!",
+    },
+    {
+      id: 998,
+      date: "Feb 3, 2025",
+      motto: "How to nest components online fast",
+      notes:
+        "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+    },
+    {
+      id: 997,
+      date: "Feb 2, 2025",
+      motto: "I'm a React Developer",
+      notes: "My React-ion when I learned about React: 😍",
+    },
+  ];
+
   return (
     <>
-      <section className="entries">
-        <h4>06. March 2024</h4>
-        <div className="entries_head">
-          <h3>Title</h3>
-          <button className="button--star">
-            <img src={starFilled}></img>
-          </button>
-        </div>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem nobis
-          alias tenetur dolor ratione libero fuga tempora. Reiciendis, corrupti
-          sunt.
-        </p>
-      </section>
+      {entries.map(({ id, date, motto, notes }) => (
+        <section key={id} className="entries">
+          <h4>{date}</h4>
+          <div className="entries_head">
+            <h3>{motto}</h3>
+            <button className="button--star">
+              <img src={starFilled}></img>
+            </button>
+          </div>
+          <p>{notes}</p>
+        </section>
+      ))}
     </>
   );
 }
